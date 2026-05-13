e81af1fe7adb0282ec7caff9d405de3ec01e6b76
agondie1705 <agdvic1705@gmail.com>
2026-05-13 10:43:46 +0200

    Create Sudoku graphical interface closes #2

--- sudoku/src/main/java/com/sudoku/Main.java
+++ sudoku/src/main/java/com/sudoku/Main.java
@@ -1,14 +1,12 @@
 package com.sudoku;
 
-import com.sudoku.model.SudokuBoard;
+import com.sudoku.ui.MainWindow;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        SudokuBoard board = new SudokuBoard();
-
-        System.out.println("Sudoku backend initialized");
+        new MainWindow();
 
     }
 }
