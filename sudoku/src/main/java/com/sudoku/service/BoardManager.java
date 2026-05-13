c507c6e3202c4d890a7d3c623180ae8bd585fcb1
agondie1705 <agdvic1705@gmail.com>
2026-05-13 17:59:42 +0200

    Refactor: create BoardManager to separate logic from UI

---
+++ sudoku/src/main/java/com/sudoku/service/BoardManager.java
@@ -0,0 +1,21 @@
+package com.sudoku.service;
+
+public class BoardManager {
+
+    public boolean isComplete(int[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public int[][] copy(int[][] original) {
+        int[][] copy = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            System.arraycopy(original[i], 0, copy[i], 0, 9);
+        }
+        return copy;
+    }
+}
